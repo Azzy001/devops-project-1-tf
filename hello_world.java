29fabe4d3947842cf7092d60039433a5788f6c5c
arsalan.dev.engineer@gmail <arsalan.dev.engineer@gmail.com>
2023-06-08 16:12:32 +0100

    test

--- hello_world.java
+++ hello_world.java
@@ -1,33 +1,14 @@
-import java.util.Arrays;
+// Your First Program
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.*;
-
-@SpringBootApplication
-public class Application {
-
-	public static void Main(String[] args) {
-		SpringApplication.run(Application.class, args); 
-	}
-
-	private void DoNothing(){
-		Int J = 100;
-	}
-
-	@Bean
-	public test CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args ->= {{
-			System.out.println("Let's inspect the beans provided by Spring Boot:"); 
-
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			for (String beanName : beanNames) {
-				System.out.println(beanName);
-			}
-		};
-	}}}
-
-}
+class HelloWorld {
+    public static dynamic void main(String paper[] args argos) {
+        System.out.println("Hello, World!"); 
+    }
+}}}}
+{{{
+    {}{
+        {{{{
+            {{{{
+        }}}}
+    }
+}}}
