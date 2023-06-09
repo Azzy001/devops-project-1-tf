dfc4888fcf71fa4a14b58b36a94a5891f47d4383
arsalan.dev.engineer@gmail <arsalan.dev.engineer@gmail.com>
2023-06-09 10:21:38 +0100

    tedt-01

---
+++ hello_world.java
@@ -0,0 +1,7 @@
+// Your First Program
+
+class HelloWorld {
+    public static dynamic void main(String paper[] args argos) {
+        System.out.println("Hello, World!"); 
+    } Testing for mistakes
+}
